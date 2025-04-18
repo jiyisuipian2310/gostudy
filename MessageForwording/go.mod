841e52a426c7e2dda7813f2e1722f2ea14309891
yull <yuliangliang@bwda.net>
2025-04-18 15:35:10 +0800

    重新实现，go 版本的透明转发, 支持客户端和服务端退出信息输出

--- MessageForwording/go.mod
+++ MessageForwording/go.mod
@@ -1,3 +1,5 @@
-module MessageForwording
+module MessageForward
 
 go 1.23.1
+
+require golang.org/x/sync v0.13.0
--- MessageForwording/main.go
+++ MessageForwording/main.go
@@ -1,81 +1,139 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"time"
+
+	"golang.org/x/sync/errgroup"
 )
+
+type ProxyInfo struct {
+	ClientConn   net.Conn
+	ClientReader *bufio.Reader
+	ClientWriter *bufio.Writer
+	ClientAddr   string
+
+	RemoteConn    net.Conn
+	RemoteReader  *bufio.Reader
+	RemoteWriter  *bufio.Writer
+	RemoteAddress string
+}
+
+func NewProxyInfo(clientconn net.Conn) *ProxyInfo {
+	return &ProxyInfo{
+		ClientConn:   clientconn,
+		ClientReader: bufio.NewReader(clientconn),
+		ClientWriter: bufio.NewWriter(clientconn),
+		ClientAddr:   clientconn.RemoteAddr().String(),
+	}
+}
+
+func (p *ProxyInfo) ClientToRemote(ctx context.Context) error {
+	defer func() {
+		//fmt.Printf("goroutine ClientToRemote exit, client %s -> remote %s\n", p.ClientAddr, p.RemoteAddress)
+	}()
+
+	buffer := make([]byte, 40960)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			p.ClientConn.SetReadDeadline(time.Now().Add(1 * time.Second))
+			len, err := p.ClientReader.Read(buffer)
+			if err != nil {
+				errmsg := ""
+				if err == io.EOF {
+					errmsg = fmt.Sprintf("客户端 %s 断开连接\n", p.ClientAddr)
+				} else if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+					//fmt.Println("Client 读取超时")
+					continue
+				} else {
+					errmsg = fmt.Sprintf("读取客户端 %s 数据失败, 原因：%s\n", p.ClientAddr, err.Error())
+				}
+				log.Println(errmsg)
+				return fmt.Errorf(errmsg)
+			}
+
+			p.RemoteWriter.Write(buffer[:len])
+			p.RemoteWriter.Flush()
+		}
+	}
+}
+
+func (p *ProxyInfo) RemoteToClient(ctx context.Context) error {
+	defer func() {
+		defer p.RemoteConn.Close()
+		//fmt.Printf("goroutine RemoteToClient exit, remote %s -> client %s\n", p.RemoteAddress, p.ClientAddr)
+	}()
+
+	buffer := make([]byte, 40960)
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			p.RemoteConn.SetReadDeadline(time.Now().Add(1 * time.Second))
+			len, err := p.RemoteReader.Read(buffer)
+			if err != nil {
+				errmsg := ""
+				if err == io.EOF {
+					errmsg = fmt.Sprintf("远端 %s 断开连接\n", p.RemoteAddress)
+				} else if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+					//fmt.Println("remote 读取超时")
+					continue
+				} else {
+					errmsg = fmt.Sprintf("读取远端 %s 数据失败, 原因：%s\n", p.RemoteAddress, err.Error())
+				}
+				log.Println(errmsg)
+				return fmt.Errorf(errmsg)
+			}
+
+			p.ClientWriter.Write(buffer[:len])
+			p.ClientWriter.Flush()
+		}
+	}
+}
+
+func handleConn(clientconn net.Conn, targetAddr string) {
+	defer func() {
+		//log.Printf("handleConn exit\n")
+		defer clientconn.Close()
+	}()
+
+	remoteconn, err := net.Dial("tcp", targetAddr)
+	if err != nil {
+		log.Println("Dial error: ", err)
+		return
+	}
+
+	proxyInfo := NewProxyInfo(clientconn)
+	proxyInfo.RemoteConn = remoteconn
+	proxyInfo.RemoteAddress = targetAddr
+	proxyInfo.RemoteReader = bufio.NewReader(remoteconn)
+	proxyInfo.RemoteWriter = bufio.NewWriter(remoteconn)
+
+	g, ctx := errgroup.WithContext(context.Background())
+	g.Go(func() error { return proxyInfo.ClientToRemote(ctx) })
+	g.Go(func() error { return proxyInfo.RemoteToClient(ctx) })
+
+	if err = g.Wait(); err != nil {
+		//log.Println("Error happened: ", err)
+	}
+}
 
 // 配置结构体
 type Config struct {
 	ListenPort int
 	TargetAddr string
-}
-
-// 加载配置
-func loadConfig() Config {
-	// 默认配置
-	config := Config{
-		ListenPort: 8080,
-		TargetAddr: "192.168.100.5:10000",
-	}
-
-	return config
-}
-
-// 处理连接
-func handleConnection(conn net.Conn, TargetAddr string) {
-	defer conn.Close()
-
-	// 连接到目标端口
-	targetConn, err := net.Dial("tcp", TargetAddr)
-	if err != nil {
-		log.Printf("无法连接到目标地址 %s: %v", TargetAddr, err)
-		return
-	}
-	defer targetConn.Close()
-
-	// 启动goroutine从客户端读取并转发到目标
-	go func() {
-		_, err := io.Copy(targetConn, conn)
-		if err != nil {
-			log.Printf("转发到目标时出错: %v", err)
-		}
-	}()
-
-	// 从目标读取并返回给客户端
-	_, err = io.Copy(conn, targetConn)
-	if err != nil {
-		log.Printf("从目标读取时出错: %v", err)
-	}
-}
-
-// 启动转发服务
-func startForwarding(listenPort int, targetAddr string) {
-	listenAddr := fmt.Sprintf("0.0.0.0:%d", listenPort)
-	listener, err := net.Listen("tcp", listenAddr)
-	if err != nil {
-		log.Fatalf("无法监听端口 %d: %v", listenPort, err)
-	}
-	defer listener.Close()
-
-	log.Printf("服务启动，监听端口 %d, 转发到本地地址 %s", listenPort, targetAddr)
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("接受连接时出错: %v", err)
-			continue
-		}
-
-		remoteAddr := conn.RemoteAddr()
-		fmt.Printf("Accepted connection from: %s, Forward to address: %s\n", remoteAddr.String(), targetAddr)
-
-		go handleConnection(conn, targetAddr)
-	}
 }
 
 // 自定义帮助信息
@@ -85,19 +143,34 @@ func Help() {
 
 func main() {
 	config := Config{}
-
 	flag.IntVar(&config.ListenPort, "listenport", 0, "本地监听端口(9000)")
 	flag.StringVar(&config.TargetAddr, "targetaddr", "", "目标地址(127.0.0.1:8080)")
 	flag.Usage = Help
-
-	// 解析命令行参数
-	flag.Parse()
+	flag.Parse() // 解析命令行参数
 
 	if config.ListenPort == 0 || config.TargetAddr == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	// 启动服务
-	startForwarding(config.ListenPort, config.TargetAddr)
+	listenAddr := fmt.Sprintf("0.0.0.0:%d", config.ListenPort)
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatalf("无法监听端口 %d: %v", config.ListenPort, err)
+	}
+
+	log.Printf("服务启动，本地监听端口 %d, 转发到远端地址 %s", config.ListenPort, config.TargetAddr)
+	defer listener.Close()
+
+	for {
+		netConn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			break
+		}
+
+		remoteAddr := netConn.RemoteAddr()
+		log.Printf("收到连接请求，客户端地址: %s, 转发地址: %s\n", remoteAddr.String(), config.TargetAddr)
+		go handleConn(netConn, config.TargetAddr)
+	}
 }
